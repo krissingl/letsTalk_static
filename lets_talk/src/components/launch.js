083297f89b9ba8408713d8441a285f10dc6f27a6
Krista Singleton <kris.singlet231@gmail.com>
2020-10-29 20:29:58 -0700

    Do some clean-up before deployment

--- lets_talk/src/components/launch.js
+++ lets_talk/src/components/launch.js
@@ -7,7 +7,7 @@ const Launch = () => (
     <p className="homeSummary">
       Would you like some practice speaking for social settings?
       <br />
-      Small talk with friends, telling stories, interviewing for a job?
+      Small talk, telling stories, interviewing for a job?
       <br />
       Talk to me and I'll hear you out! Select the "Let's Have Tea" tab at the top to get started!
     </p>
